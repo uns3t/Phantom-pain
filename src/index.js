e86b0630513e56cffb5ba85bedb3d68f768f2214
uns3t <uns3tzhang@gmail.com>
2019-11-04 18:11:16 +0800

    后端架构

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from "./router.js";
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+import { createBrowserHistory } from "history";
+import {Router, Route, Switch} from "react-router-dom";
+import Home from "./components/home/Home.js"
+import Admin from "./components/admin/Admin.js"
+import Ideadetail from "./components/ideadetail/Ideadetail.js"
+import Idealist from "./components/idealist/Idealist.js"
+
+const routerhistory=createBrowserHistory()
+
+ReactDOM.render(
+    <Router history={routerhistory}>
+        <Switch>
+            <Route path="/admin" component={Admin}/>
+            <Route path="/ideadetail" component={Ideadetail}/>
+            <Route path="/idealist" component={Idealist}/>
+            <Route path="/" component={Home}/>
+
+        </Switch>
+    </Router>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
